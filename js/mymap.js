1826928f58619197a50d01402cc627053a812538
ezangakis <41299158+ezangakis@users.noreply.github.com>
2021-09-02 11:51:07 -0400

    Update mymap.js
    Updated mapbox api pull

--- js/mymap.js
+++ js/mymap.js
@@ -1,13 +1,23 @@
 /* Create the map */
 var mymap = L.map('mapid').setView([39.9526, -75.1652], 6);
-	
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZXphbmciLCJhIjoiY2prYmZ4MWRmMHdpeTNxdDRobzlzdzhqYiJ9.gH8NQVABBFmrMzf4IVX5fQ'
+}).addTo(mymap);
+/*
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
    	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom:18,
    	id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZXphbmciLCJhIjoiY2prYmZ4MWRmMHdpeTNxdDRobzlzdzhqYiJ9.gH8NQVABBFmrMzf4IVX5fQ'
 }).addTo(mymap);
-
+*/
 /* Define icon class */
 var LeafIcon = L.Icon.extend({
     options: {
